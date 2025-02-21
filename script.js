564a2a020df04a3fe585eeb3edcb3356770fccba
Kris-tina30 <kristina.pichkar.15@gmail.com>
2025-02-21 11:38:31 +0100

    add styles, class to item,

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-// 2- Add a button click event listener using JavaScript that performs the following
-// actions when the submit button is clicked:
-
-// Prevent the form from submitting and refreshing the page using e.preventDefault().
-// Change the background color of the form (#my-form) to red.
-// Add a new class called bg-dark to the <body> element. This class should change
-// the background color of the entire webpage to dark (e.g., black or dark gray) and
-// change the text color to white.
-// Replace the last <li> element in the unordered list (ul.items) with an <h1>
-// element that says "Hello".
 const btn = document.querySelector(".btn");
 console.log(btn);
 btn.addEventListener("click", function (e) {
-  e.preventDefault(); // Prevents the form from submitting and refreshing the page
+  e.preventDefault();
 
-  document.querySelector("#my-form").style.background = "red"; // Changes form background to red
-  document.querySelector("body").classList.add("bg-dark"); // Adds 'bg-dark' class to body (changes background to dark)
+  document.querySelector("#my-form").style.background = "red";
+
+  const body = document.querySelector("body");
+  const listItems = document.querySelectorAll("li");
+
+  if (!body.classList.contains("bg-dark")) {
+    body.classList.add("bg-dark");
+    listItems.forEach((item) => {
+      item.classList.add("bg-dark");
+    });
+  } else {
+    body.classList.remove("bg-dark");
+    listItems.forEach((item) => {
+      item.classList.remove("bg-dark");
+    });
+  }
+
   document.querySelector(".items").lastElementChild.innerHTML =
-    "<h1>Hello</h1>"; // Changes the last <li> to a <h1>Hello</h1>
+    "<h1>Hello</h1>";
 });
